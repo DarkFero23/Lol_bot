445ced271d6d14a434792a507f094c156b045357
DarkFero23 <luizln760@gmail.com>
2024-09-02 00:22:02 -0500

    osymandiass

--- probar.py
+++ probar.py
@@ -3,7 +3,7 @@ import time
 
 while True:
     try:
-        if pyautogui.locateOnScreen('C:/Users/Luis/Downloads/Lol_bot/Lol_bot/BACKEND/Personajes_pick\leblanc.png', confidence=0.7) is not None:
+        if pyautogui.locateOnScreen('C:/Users/Luis/Downloads/Lol_bot/Lol_bot/BACKEND/Personajes_pick/leblanc.png', confidence=0.7) is not None:
             print("Se ve")
         else:
             print("No se ve")
